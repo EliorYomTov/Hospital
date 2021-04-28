b11964b59bdf95106596fc0e71223ac523a35210
EliorYomTov <39540940+EliorYomTov@users.noreply.github.com>
2021-04-28 17:30:29 +0300

    Delete Hospital directory

--- Hospital/src/Run.java
+++
@@ -1,8 +0,0 @@
-public class Run {
-	public static void main(String[] args) {
-		PrintUtils.wellcome();
-		Hospital hospital = new Hospital("Ichilov Hospital");
-		PrintUtils.showHospitalDetails(hospital);
-		PrintUtils.showHospitalStatistics(hospital);
-	}
-}
--- Hospital/src/Utils.java
+++
@@ -1,6 +0,0 @@
-
-public class Utils {
-	public static int generateRandom(int min, int max) {
-		return (int) (Math.random() * max - min) + min;
-	}
-}
